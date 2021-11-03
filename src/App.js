0813a8084e4602d8b3d3e3b709fb8e49f1b4c228
Miljana Botunjac <miljanabotunjac@gmail.com>
2021-11-03 15:49:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import DragNDrop from './components/DragNDrop';
+
+const data = [
+  {title: 'Backlog', items: ['1', '2', '3']},
+  {title: 'In Progress', items: ['4', '5']},
+  {title: 'Complete', items: ['6', '7']},
+  {title: 'On Hold', items: []}
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <DragNDrop data = {data}/>
       </header>
     </div>
   );
